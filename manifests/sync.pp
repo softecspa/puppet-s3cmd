dceb79755b0a80c97f08a72d760fb2188e98bebd
lorello <lorenzo.salvadorini@softecspa.it>
2015-03-31 17:31:11 +0200

    added lazy mode

--- manifests/sync.pp
+++ manifests/sync.pp
@@ -23,15 +23,27 @@
 #
 # [*delete*]
 #   Specifies if you want remove remote files, removed locally
+#   default: false
+#
+# [*lazy*]
+#   In lazy mode, the sync happens only if $source contain files changed from
+#   the last puppet run. Default: false.
+#
 define s3cmd::sync (
   $source,
   $bucket_name,
   $single  = false,
   $prefix  = false,
   $reverse = false,
-  $delete  = false
+  $delete  = false,
+  $lazy    = false, 
   )
 {
+  validate_bool($single)
+  validate_bool($reverse)
+  validate_bool($delete)
+  validate_bool($lazy)
+
   $real_prefix = $prefix ? {
     false      => '',
     default => $prefix,
@@ -58,9 +70,19 @@ define s3cmd::sync (
       require => File[$source],
     }
   } else {
+      
+      if $lazy {
+        $exec_conditions = [
+          "stat ${source}",
+          "find ${source} -type f -newer /var/lib/puppet/state/state.yaml"
+        ]
+      } else {
+        $exec_conditions = "stat ${source}"
+      }
+
       exec { "sync $source --> s3://$bucket_name/$real_prefix":
         command => "s3cmd sync --no-progress $real_delete $source s3://$bucket_name/$real_prefix",
-        onlyif  => "stat $source",
+        onlyif  => $exec_conditions
       }
   }
 }
